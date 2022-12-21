d70b02fe041694dcd336059fc4115dfe2f0106e0
Benjamin <blzwicker@outlook.com>
2022-12-20 23:25:01 -0400

    Adding dashboard tests

--- pages/company_select.page.ts
+++ pages/company_select.page.ts
@@ -3,12 +3,10 @@ import * as companySelectPageData from '../data/company_select_page.data.json';
 
 export class CompanySelectPage {
   readonly page: Page;
-  readonly companiesHeader: Locator;
   readonly plooto: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.companiesHeader = page.locator('a', { hasText: companySelectPageData.companies_header });
     this.plooto = page.getByRole('row').getByText(companySelectPageData.companies.plooto).first();
   }
 
